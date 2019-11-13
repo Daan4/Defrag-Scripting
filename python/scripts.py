88f238c7358a3d171748039afc58cf61d069f18d
Daan4 <daan_lubbers@hotmail.com>
2019-11-13 21:15:54 +0100

    Simple replayer that records and plays back gameplay

--- python/scripts.py
+++ python/scripts.py
@@ -1,10 +1,49 @@
 from constants import *
-
+import csv
+import logging
 
 # Return a list of scripts to run
 def init_scripts():
-    x = TestScript()
+    #x = TestScript()
+    x = LiveRecorderScript("test.csv")  # 15:104
+    #x = ReplayScript("test.csv")
     return [x]
+
+
+class DemoRecorderScript:
+    pass
+
+
+class LiverecorderScript:
+    def __init__(self, filename):
+        self.csv_writer = csv.writer(open(filename, "w", newline=''))
+
+    def run(self, callback_name, *args):
+        if callback_name == "CL_CreateCmd":
+            return self.write(*args)
+
+    def write(self, *args):
+        self.csv_writer.writerow(args)
+        return args
+
+
+class ReplayScript:
+    def __init__(self, filename):
+        self.csv_reader = csv.reader(open(filename, "r"))
+
+    def run(self, callback_name, *args):
+        if callback_name == "CL_CreateCmd":
+            return self.read(*args)
+
+    def read(self, server_time, *args):
+            # keep reading from csv file until we reached the end, then pass user input again
+        if self.csv_reader is not None:
+            try:
+                row = list(map(int, next(self.csv_reader)))
+                return tuple([server_time] + row[1:])
+            except StopIteration:
+                self.csv_reader = None
+        return tuple([server_time] + list(args))
 
 
 class TestScript:
